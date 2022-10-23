5f874289f3bbe0c9f02e28bab264ebb89326b689
VuHoi <toilahoi007@gmail.com>
2022-10-23 16:46:33 +0700

    fix: css and add mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 // import customStaticProps from '@utils/staticProps';
-import { DefaultLayout } from '@features/layout/components/DefaultLayout';
-import React from 'react';
+import { DefaultLayout } from "@features/layout/components/DefaultLayout";
+import Button from "@mui/material/Button";
+import React from "react";
 
 export default function Home() {
-  return <DefaultLayout>Home</DefaultLayout>
+  return (
+    <DefaultLayout>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </DefaultLayout>
+  );
 }
 
 // export const getStaticProps = customStaticProps(null, null);
