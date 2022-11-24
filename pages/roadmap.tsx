ea81d18df0708768e7261b50b3400f015b0b7158
VuHoi <toilahoi007@gmail.com>
2022-11-25 01:26:05 +0700

    feat: init editor

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,9 +1,24 @@
 // import customStaticProps from '@utils/staticProps';
+import ContentEditable from "@components/contenteditable";
 import { DefaultLayout } from "@features/layout/components/DefaultLayout";
-import React from "react";
-
+import React, { useRef, useState } from "react";
 export default function Home() {
-  return <DefaultLayout></DefaultLayout>;
+  const ref = useRef();
+  const [html, sethtml] = useState("<b>Hello <i>World</i></b>");
+  const handleChange = (evt: any) => {
+    sethtml(evt.target.value + "<h1>aaa</h1>");
+  };
+  return (
+    <DefaultLayout>
+      <ContentEditable
+        innerRef={ref as any}
+        html={html} // innerHTML of the editable div
+        disabled={false} // use true to disable editing
+        onChange={handleChange} // handle innerHTML change
+        tagName="article" // Use a custom HTML tag (uses a div by default)
+      />
+    </DefaultLayout>
+  );
 }
 
 // export const getStaticProps = customStaticProps(null, null);
